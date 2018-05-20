a9e4b03faaacbb70cb6c7bf4f73b07744ba181e5
Okens <Ng.Eric314@gmail.com>
2018-05-19 21:32:31 -0700

    Updating spotify command to show author, song, and url. Including default to 'The Sign' by 'Ace of Base'

--- javascript/liri.js
+++ javascript/liri.js
@@ -13,37 +13,84 @@ var client = new Twitter(keys.twitter);
 
 
 var commandName = process.argv[2];
+var inputArgs = process.argv;
 
+var inputInfo = "";
+
+for (var i = 3; i < inputArgs.length; i++) {
+    inputInfo += inputArgs[i] + "+";
+}
+
+inputInfo = inputInfo.substring(0, inputInfo.length - 1);
 
 console.log(commandName);
+console.log(inputInfo);
 
 if (commandName === "my-tweets") {
-
+    console.log("Entered Tweets")
+    var params = { screen_name: 'Omens68390424' };
+    // client.get("statuses/user_timeline", params, function(error, tweets, response){
+    //     if (!error) {
+    //         console.log(tweets);
+    //         console.log(response);
+    //     }
+    //     console.log(error);
+    // })
 
 } else if (commandName === "spotify-this-song") {
+
+    if (inputInfo === "") {
+        inputInfo = "Complicated";
+        console.log("Default input: " + inputInfo);
+    };
+
     spotify.search({
+        // limit: "10",
         type: 'track',
-        query: 'All the Small Things'
+        query: inputInfo
     }, function (err, data) {
-        if(err){
+        if (err) {
             return console.log("Error occurred: " + err);
+        } else if (data["tracks"]["total"] != 0) {
+
+            // var tracks = data["tracks"]["items"];
+            // console.log(data);
+            var dataTracks = data["tracks"]["items"];
+
+            for (var h = 0; h < dataTracks.length; h++) {
+                console.log("===================================================");
+                console.log("Song Name: " + dataTracks[h]["name"]);
+
+                for (var i = 0; i < dataTracks[h]["artists"].length; i++) {
+                    console.log("Song Artist " + (i + 1) + ": " + dataTracks[h]["artists"][i]["name"]);
+                }
+
+                console.log("Song URL: " + dataTracks[h]["external_urls"]["spotify"]);
+            }
+        } else {
+            //Avoiding another search due to possibility of endless loop if the sign does not exist as a result.
+            console.log("===================================================");
+            console.log("Song Name: " + "The Sign");
+            console.log("Song Artist " + "Ace of Base");
+            console.log("Song URL: " + "https://open.spotify.com/track/0hrBpAOgrt8RXigk83LLNE");
         }
-        console.log(data);
     });
 
 } else if (commandName === "movie-this") {
 
+
+
 } else if (commandName === "do-what-it-says") {
 
 } else {
-    console.log("==========================================");
+    console.log("===================================================");
     console.log("Sorry that is not a proper command");
     console.log("Please use one of the below: ");
     console.log("my-tweets");
     console.log("spotify-this-song");
     console.log("movie-this");
     console.log("do-what-it-says");
-    console.log("==========================================");
+    console.log("===================================================");
 }
 
 
