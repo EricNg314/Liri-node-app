39f41804dc3eecf0dee74841da2707adfd50221b
Okens <Ng.Eric314@gmail.com>
2018-06-14 22:50:14 -0700

    Moving all items up a directory level.

--- liri.js
+++ liri.js
@@ -5,7 +5,7 @@ var Movie = require("request");
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
 
-var keys = require('../javascript/keys.js');
+var keys = require('./keys.js');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
